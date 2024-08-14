5a592eaa89f9c730bf9830626f6de4d420ab80e5
febeFlo <steffanyflorence@gmail.com>
2024-08-14 10:44:31 +0700

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Selamat Datang di Suara Yogyakarta</h1>
+
+    <h2>Title</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/izrk-erhDdk?si=kjkmpShT5LZijRsg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</body>
+</html>
