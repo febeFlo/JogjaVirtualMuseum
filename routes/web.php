a2c3ca8cb40549bb71e1e3feb614fe5e44345581
Evan Tanuwijaya <etanuwijaya96@gmail.com>
2024-09-10 22:12:45 +0700

    evan Route

--- routes/web.php
+++ routes/web.php
@@ -50,4 +50,4 @@ Route::post('/submit1', [Kuesioner1Controller::class, 'submit'])->name('submit1'
 Route::post('/submit2', [Kuesioner34Controller::class, 'submit'])->name('submit2');
 
 
-// Master Route
+// Evan Route
