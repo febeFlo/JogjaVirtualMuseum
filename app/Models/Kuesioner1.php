e68e23bd9f68593cfef5883da654403cfc195754
Evan Tanuwijaya <etanuwijaya96@gmail.com>
2024-09-10 21:13:13 +0700

    add relation one to one

--- app/Models/Kuesioner1.php
+++ app/Models/Kuesioner1.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Kuesioner1 extends Model
 {
@@ -22,5 +23,25 @@ class Kuesioner1 extends Model
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function lokasi1Map(): HasOne {
+        return $this->hasOne(Map::class, 'id', 'lokasi1');
+    }
+
+    public function lokasi2Map(): HasOne {
+        return $this->hasOne(Map::class, 'id', 'lokasi2');
+    }
+
+    public function lokasi3Map(): HasOne {
+        return $this->hasOne(Map::class, 'id', 'lokasi3');
+    }
+
+    public function lokasi4Map(): HasOne {
+        return $this->hasOne(Map::class, 'id', 'lokasi4');
+    }
+
+    public function lokasi5Map(): HasOne {
+        return $this->hasOne(Map::class, 'id', 'lokasi5');
     }
 }
